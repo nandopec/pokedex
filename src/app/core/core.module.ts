4defbe38d2b9a3a62cea0e43a411422672e7ba35
nandopec <nandopec.23@gmail.com>
2023-08-10 11:00:39 -0600

    feat(core): Alias added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from 'src/environments/environment';
+import { environment } from '@env/environment';
 
 @NgModule({
     declarations: [],
